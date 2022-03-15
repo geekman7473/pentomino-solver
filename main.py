ea601f2acd6063fe5d60f3b2a1751756a5bde17f
Justin Becker <geekman7473@gmail.com>
2022-03-15 02:12:54 -0700

    optimizations!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import copy
 import scipy.ndimage
 import numpy as np
 import random
+from numba import jit
 
 ROWS = 10
 COLS = 6
@@ -13,6 +14,8 @@ PIECE_FILE_PATH = "pieces.txt"
 Total_Search_Space = 0
 Remaining_Search_Space = 0
 SOLNS_FILE_PATH = "solns.txt"
+number_of_found_solns = 0
+configs_top_left_cache = {}
 
 SMALLEST_PIECE_SIZE = 5  #TODO: find algorithmically
 
@@ -20,6 +23,7 @@ def convert_to_bitboard(board):
     bit_board = np.copy(board)
     bit_board[bit_board > 0] = 1
     return bit_board
+
 
 def load_pieces_from_file(piece_file_path):
     pieces_np = []
@@ -82,22 +86,21 @@ def is_board_valid(board):
     # if we find that there is contiguous empty region smaller than 5,
     # reject the board
     for slices in empty_spaces:
-        if np.sum(anti_bit_board[slices]) < SMALLEST_PIECE_SIZE:
+        if np.sum(anti_bit_board[slices]) % SMALLEST_PIECE_SIZE != 0:
             return False
     # We will check contiguous regions of the same size or higher if needed
     return True  # THIS MAY NOT BE RIGHT
 
 
 def left_top_most_space(arr, is_board):
-    if is_board:
-        arr = convert_to_bitboard(arr)
-        arr = 1 - arr
+    target = 0 if is_board else 1
+
     best_y = inf
     best_x = inf
     # look for zeros
     for y in range(arr.shape[0]):
         for x in range(arr.shape[1]):
-            if arr[y,x] == 1:
+            if arr[y,x] == target:
                 if x < best_x:
                     best_x = x
                     best_y = y
@@ -110,20 +113,31 @@ def left_top_most_space(arr, is_board):
     return (best_y, best_x)
 
 
-def attempt_piece_placement(board, piece, piece_number):
-    board_coords = left_top_most_space(board, True)
-    piece_coords = left_top_most_space(piece, False)
+def init_configs_top_left_cache(unique_configs):
+    global configs_top_left_cache
+    for i in range(len(unique_configs)):
+        for j in range(len(unique_configs[i])):
+            configs_top_left_cache[(i,j)] = left_top_most_space(unique_configs[i][j], False)
+
+
+@jit
+def attempt_piece_placement(board, piece, piece_number, board_top_left, piece_top_left):
+    #board_coords = left_top_most_space(board, True)
+    board_coords = board_top_left
+    piece_coords = piece_top_left
 
     board_rows = board.shape[0]
     board_cols = board.shape[1]
     piece_rows = piece.shape[0]
     piece_cols = piece.shape[1]
 
+    board_rows_range = range(0, board_rows)
+    board_cols_range = range(0, board_cols)
     # Check if we will go off the board
-    if board_coords[0] - piece_coords[0] not in range(0, board_rows)\
-        or board_coords[0] + ((piece_rows - 1) - piece_coords[0]) not in range(0, board_rows)\
-        or board_coords[1] - piece_coords[1] not in range(0, board_cols)\
-        or board_coords[1] + ((piece_cols - 1) - piece_coords[1]) not in range(0, board_cols):
+    if board_coords[0] - piece_coords[0] not in board_rows_range\
+        or board_coords[0] + ((piece_rows - 1) - piece_coords[0]) not in board_rows_range\
+        or board_coords[1] - piece_coords[1] not in board_cols_range\
+        or board_coords[1] + ((piece_cols - 1) - piece_coords[1]) not in board_cols_range:
         return False, board
 
     # modified board
@@ -134,7 +148,7 @@ def attempt_piece_placement(board, piece, piece_number):
         for j in range(piece_cols):
             y = board_coords[0] + (i - piece_coords[0])
             x = board_coords[1] + (j - piece_coords[1])
-            assert y >= 0 and x >= 0
+
             if piece[i,j] != 0 and mod_board[y,x] != 0:
                 return False, board
             # this covers the conditions where mod_board[y,x] == 1 and piece[i,j] == 0, 
@@ -144,6 +158,7 @@ def attempt_piece_placement(board, piece, piece_number):
 
     return True, mod_board
 
+
 def search_space_size(unique_configs):
     size = factorial(len(unique_configs))
     for config_set in unique_configs.values():
@@ -151,9 +166,12 @@ def search_space_size(unique_configs):
 
     return size
 
+
 def recursive_descent(board, pieces_left, recursion_depth, solns_file):
     global Total_Search_Space
     global Remaining_Search_Space
+    global number_of_found_solns
+    global configs_top_left_coords
     if len(pieces_left) == 0:
         print("Solution found!")
         print(board)
@@ -162,20 +180,26 @@ def recursive_descent(board, pieces_left, recursion_depth, solns_file):
         solns_file.write(str((Remaining_Search_Space/Total_Search_Space) * 100) + "%\n")
         solns_file.write("\n------------------------------\n")
         solns_file.flush()
-        #quit()
+        #number_of_found_solns += 1
+        #if number_of_found_solns == 100:
+            #quit()
+
+    board_coords = left_top_most_space(board, True)
+
     for i in pieces_left.keys():
         for j in range(len(pieces_left[i])):
             config = pieces_left[i][j]
-            succeeded, new_board = attempt_piece_placement(board, config, i + 1)
+            succeeded, new_board = attempt_piece_placement(board, config, i + 1, board_coords, configs_top_left_cache[(i,j)])
             new_pieces_left = pieces_left.copy() # This should be faster than copy.deepcopy 
                                                  # and we also don't need deep copy here
-            new_pieces_left.pop(i)
+
             if succeeded and is_board_valid(new_board):
+                new_pieces_left.pop(i)
                 recursive_descent(new_board, new_pieces_left, recursion_depth + 1, solns_file)
             else:
                 Remaining_Search_Space -= search_space_size(new_pieces_left)
-                if random.randint(0,10000) == 10:
-                    print((Remaining_Search_Space/Total_Search_Space) * 100)
+                #if random.randint(0,10000) == 10:
+                    #print((Remaining_Search_Space/Total_Search_Space) * 100)
 
     return
 
@@ -185,6 +209,7 @@ def main():
     global Remaining_Search_Space
     pieces = load_pieces_from_file("pieces.txt")
     unique_configs = generate_unique_piece_configurations(pieces)
+    init_configs_top_left_cache(unique_configs)
     Total_Search_Space = search_space_size(unique_configs)
     Remaining_Search_Space = Total_Search_Space
 
